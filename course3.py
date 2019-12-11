ae53a219aaedb165a0858c18a99b927e7db175bd
chniranjanreddy <chniranjanreddy@gmail.com>
2019-12-11 12:34:38 +0100

    added course3.py file

---
+++ course3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 16:27:35 2019
+
+@author: chniranjanreddy new source code
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+#read excel file
+df = pd.read_excel ("population_total (1).xlsx",0) #read excel
